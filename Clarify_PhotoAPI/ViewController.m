501eb90f5d438372504ec80ce87225252e9adbd7
Urvashi-iPhone <lathiyaurvashi36@gmail.com>
2016-12-24 18:18:02 +0530

    first

--- Clarify_PhotoAPI/ViewController.m
+++ Clarify_PhotoAPI/ViewController.m
@@ -7,18 +7,73 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
-
+#import "ClarifaiApp.h"
+@interface ViewController ()< UIImagePickerControllerDelegate>
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+@property (weak, nonatomic) IBOutlet UIButton *button;
+@property (strong, nonatomic) ClarifaiApp *app;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
 }
 
+- (IBAction)buttonPressed:(id)sender {
+    // Show a UIImagePickerController to let the user pick an image from their library.
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    picker.allowsEditing = NO;
+    picker.delegate = self;
+    [self presentViewController:picker animated:YES completion:nil];
+}
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    [self dismissViewControllerAnimated:YES completion:nil];
+    UIImage *image = info[UIImagePickerControllerOriginalImage];
+    if (image) {
+        // The user picked an image. Send it to Clarifai for recognition.
+        self.imageView.image = image;
+        self.textView.text = @"Recognizing...";
+        self.button.enabled = NO;
+        [self recognizeImage:image];
+    }
+}
+
+- (void)recognizeImage:(UIImage *)image {
+    
+    // Initialize the Clarifai app with your app's ID and Secret.
+    ClarifaiApp *app = [[ClarifaiApp alloc] initWithAppID:@"VqbRvVMvl4FqbP2b6q3EGOdSwaOGZRAPXMmLRTL5"
+                                                appSecret:@"HuplowPo__D60zihpR4k_8JM7aU53_qD8uGCneLd"];
+    
+    // Fetch Clarifai's general model.
+    [app getModelByName:@"general-v1.3" completion:^(ClarifaiModel *model, NSError *error) {
+        // Create a Clarifai image from a uiimage.
+        ClarifaiImage *clarifaiImage = [[ClarifaiImage alloc] initWithImage:image];
+        
+        // Use Clarifai's general model to pedict tags for the given image.
+        [model predictOnImages:@[clarifaiImage] completion:^(NSArray<ClarifaiOutput *> *outputs, NSError *error) {
+            if (!error) {
+                ClarifaiOutput *output = outputs[0];
+                
+                // Loop through predicted concepts (tags), and display them on the screen.
+                NSMutableArray *tags = [NSMutableArray array];
+                for (ClarifaiConcept *concept in output.concepts) {
+                    [tags addObject:concept.conceptName];
+                }
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    self.textView.text = [NSString stringWithFormat:@"Tags:\n%@", [tags componentsJoinedByString:@", "]];
+                });
+            }
+        }];
+    }];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
